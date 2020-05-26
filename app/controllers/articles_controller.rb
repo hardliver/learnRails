d03a0405e5b92c6618efdbab0dc317beddc44729
hardliver <yelionel26@gmail.com>
2020-05-26 08:14:46 +0800

    Edit and update: update existing articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,6 +12,10 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
   
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
@@ -21,7 +25,16 @@ class ArticlesController < ApplicationController
     else
       render 'new'
     end
+  end
 
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = 'Article was edited successfully'
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
   
 end
