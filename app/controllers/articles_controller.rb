155fa3c08318e9c1e0294dddc91c69cd67f577a3
hardliver <yelionel26@gmail.com>
2020-05-26 08:45:27 +0800

    Delete: delete articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -36,5 +36,11 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
-  
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to @article
+  end
+
 end
